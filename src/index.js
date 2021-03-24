fd34ab739ed25ea61b5b7730f3eac0a8e4e9e725
amandahp <amandahoffmann96@gmail.com>
2021-03-24 12:52:23 -0300

    feat:added loader html and txt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Title from './components/title'
 import Image from './components/image'
 import Button from './components/button'
+import Warning from './templates/warning.html'
+import "./styles/warning.css"
+import fraseTxt from './files/frase.txt'
 
 const title = new Title();
 
@@ -13,3 +16,24 @@ image.insertBobImage();
 const button = new Button();
 
 button.create();
+
+//Babel spread (coloca todo o resto que sobra, dentro de um objeto)
+
+const obj = { a: 1, b: 2, c: 3, d: 4}
+
+let {a, b, ...teste} = obj;
+
+console.log(a)
+console.log(b)
+console.log(teste)
+
+//import de html
+
+const body = document.querySelector('body');
+body.innerHTML += Warning;
+
+//import arquivo de texto
+const frase = fraseTxt;
+
+console.log(frase)
+console.log(frase.toUpperCase())
